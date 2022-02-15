ff826d9be1d806122379e5267328e670f8f96136
Hayden Kidd <hkidd@live.co.uk>
2022-02-15 15:02:22 +0000

    added routes

--- server.py
+++ server.py
@@ -4,9 +4,17 @@ from flask_cors import CORS
 server = Flask(__name__)
 CORS(server)
 
-@server.route('/')
-def welcome():
+@server.route('/', methods=['GET', 'POST'])
+def login():
     return render_template('home.html') 
+
+@server.route('/create', methods=['GET', 'POST'])
+def create_message():
+    return render_template('create.html')
+
+@server.route('/decode', methods=['GET', 'POST'])
+def decode_message():
+    return render_template('decode.html')
 
 if __name__ == '__main__':
     server.run(debug=True)
